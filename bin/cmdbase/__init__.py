df0b0a444a25dc6ed3e006747dabea04713e0627
Vijaya Baskar <vjbaskar@gmail.com>
2024-08-21 07:56:09 +0100

    Added a history decorator for recording command events

---
+++ bin/cmdbase/__init__.py
@@ -0,0 +1,1 @@
+from .helpers.random_an import *
---
+++ bin/cmdbase/helpers/random_an.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import random
+import string
+
+def generate_random_name(length=8):
+    # Define the characters to choose from (letters and digits)
+    characters = string.ascii_letters + string.digits
+
+    # Generate a random name
+    random_name = ''.join(random.choice(characters) for _ in range(length))
+
+    return random_name
