1bc9e66c92181da992f602cc7c0d3271389d1d13
Dave Horsfall <dave@horsfall.dev>
2024-10-15 15:06:21 +0100

    version test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,20 @@ from click.testing import CliRunner
 from solosis.cli import cli
 
 
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--version"])
+    assert result.exit_code == 0
+    assert "version 0.1.0" in result.output
+
+
+def test_help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--help"])
+    assert result.exit_code == 0
+    assert "Show this message and exit" in result.output
+
+
 def test_greet():
     runner = CliRunner()
     result = runner.invoke(cli, ["command-one", "--name", "Alice"])
