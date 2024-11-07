c8c13c9bcb60a0a354c53de664d4cc2b3c72a18f
Louise Grimble <louiseegrimble@gmail.com>
2024-11-07 09:52:34 +0000

    refactor work for #29

--- bin/irods/pull-fastqs/submit.sh
+++ bin/irods/pull-fastqs/submit.sh
@@ -72,13 +72,13 @@ nextflow run cellgeni/nf-irods-to-fastq -r main main.nf \
 # Clean up the temporary sample file after Nextflow completes
 rm -f "$SAMPLE_FILE"
 
-#re-adding structure to outputs
-KK=`ls *fastq.gz | sed "s/_S/\t/" | cut -f1 | sort | uniq`
+# Re-adding structure to outputs
+KK=$(ls *fastq.gz | sed "s/_S[0-9]\+.*//" | sort | uniq)
 mkdir -p $KK 
 
 for i in $KK
 do
-  mv $i*fastq.gz $i
+  mv ${i}*fastq.gz $i
 done
 
 echo "All samples processed with Nextflow."
