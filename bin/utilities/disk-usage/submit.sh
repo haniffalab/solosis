22bb03b7206cc366109538f30ace186335933cf1
Louise Grimble <louiseegrimble@gmail.com>
2024-11-28 17:26:49 +0000

    new command disk-usage #43

---
+++ bin/utilities/disk-usage/submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# submit.sh - Environment setup for disk-usage
+
+# Usage:
+#   ./submit.sh 
+#
+# Parameters:
+#   <change-this> - NOTHING ESSENTIAL
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+#removing for now.. chatGPT has a decent response to this though
+# Ensure at least one argument is provided
+#if [ "$#" -lt 1 ]; then
+#  echo "Usage: $0" >&2
+#  exit 1
+#fi
+
+nfs_overall=$(df -h /nfs/team298)
+
+
+echo "printing nfs usage: $nfs_overall"
---
+++ solosis/commands/utilities/disk_usage.py
@@ -0,0 +1,73 @@
+import os
+import subprocess
+
+import click
+
+from solosis.utils import echo_message
+
+
+@click.command("disk-usage")
+# @click.option("--workspace", type=str, required=True, help="Name of the workspace to check disk usage.")
+def cmd():
+    """
+    Check disk usage for a specified workspace.
+
+    This command retrieves the disk usage for a given workspace directory
+    across NFS, Lustre, or warehouse storage systems.
+    """
+    # Print an introductory message
+    echo_message(
+        f"Launching: {click.style('disk-usage', bold=True, underline=True)}",
+        "info",
+    )
+
+    # not sure this is necessary
+    # Get the base directory for the team workspaces
+    team_workspace_dir = os.getenv(
+        "TEAM_WORKSPACE_DIR", "/lustre/scratch126/cellgen/team298/workspaces"
+    )
+
+    # Ensure the base directory exists
+    if not os.path.isdir(team_workspace_dir):
+        echo_message(
+            f"Workspace directory '{team_workspace_dir}' does not exist.",
+            "error",
+        )
+        return
+
+    # Path to the Cell Ranger submission script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    diskusage_submit_script = os.path.abspath(
+        os.path.join(script_dir, "../../../bin/utilities/disk-usage/submit.sh")
+    )
+    # Construct the command with optional BAM flag
+    cmd = [diskusage_submit_script]
+
+    ####this may not be relevant now..
+    # if not os.path.exists(workspace_path):
+    #    echo_message(
+    #        f"Workspace '{workspace}' does not exist at path '{workspace_path}'.",
+    #        "error",
+    #    )
+    #    return
+
+    # Run the cmd to calculate disk usage
+    try:
+        echo_message(f"Calculating disk usage for...", "progress")
+        result = subprocess.run(
+            cmd,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+        echo_message(f"Disk usage for workspace: {result.stdout.strip()}", "success")
+    except subprocess.CalledProcessError as e:
+        echo_message(
+            f"Error while calculating disk usage: {e.stderr.strip()}",
+            "error",
+        )
+
+
+if __name__ == "__main__":
+    cmd()
