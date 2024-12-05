99104110a7f7668b6be6006292cabd30ba9260e2
Louise Grimble <louiseegrimble@gmail.com>
2024-12-05 16:22:55 +0000

    new command filecount #47

--- bin/utilities/filecount/submit.sh
+++ bin/utilities/filecount/submit.sh
@@ -7,6 +7,25 @@
 # Parameters:
 #   <change-this> - NOTHING ESSENTIAL
 
+# Define a function to generate an HTML table
+generate_html_table() {
+    local data=("$@")
+
+    # Begin HTML table
+    echo "<html><body>"
+    echo "<table border='1' cellpadding='5' cellspacing='0'>"
+    echo "<tr><th>Workspace</th><th>File Count</th><th>File Limit</th></tr>"
+
+    # Add rows
+    for row in "${data[@]}"; do
+        # Split the row into columns
+        echo "<tr><td>${row// /</td><td>}</td></tr>"
+    done
+
+    # End HTML table
+    echo "</table>"
+    echo "</body></html>"
+
 # lustre
 #lfs quota -g team298 -h /lustre/scratch126
 file_count=$(lfs quota -g team298 -h /lustre/scratch126 | sed -n '4p' | awk '{print $5}')
@@ -15,45 +34,49 @@ file_limit=$(lfs quota -g team298 -h /lustre/scratch126 | sed -n '4p' | awk '{pr
 # nfs
 # df -h /nfs/team298
 #nfs_count=$(find /nfs/team298 -type f | wc -l)
+nfs_count=345678
+nfs_lim=1500000
 
 #warehouse
 #df -h /warehouse/team298_wh01
 wh_count=$(find /warehouse/team298_wh01 -type f | wc -l)
-
+wh_lim=150000
 
 # Array of data
-data=("Lustre $file_count $file_limit "
-        "nfs nfs_count nfs_lim "
-        "warehouse $wh_count wh_lim ")
+data=(
+    "Lustre $file_count $file_limit"
+    "nfs $nfs_count $nfs_lim"
+    "warehouse $wh_count $wh_lim"
+)
 
-GROUP="team298"
-CPU=2
-MEM=3000
-Q="normal"
-TEAM_LOGS_DIR="$HOME/logs"
+# Generate the HTML table
+html_table=$(generate_html_table "${data[@]}")
 
-bsub -J "filecount" <<EOF
-#!/bin/bash
-#BSUB -n $CPU \
-#BSUB -M $MEM \
-#BSUB -R"span[hosts=1] select[mem>${MEM}] rusage[mem=${MEM}]" \
-#BSUB -G $GROUP \
-#BSUB -q $Q \
-#BSUB -o "$TEAM_LOGS_DIR/filecount.out" \
-#BSUB -e "$TEAM_LOGS_DIR/filecount.err" \
-
-# Define headers
-printf "%-12s %-10s %-10s \n" "workspace" "filecount" "file-limit"
-printf "%-12s %-10s %-10s \n" "--------" "--------" "--------"
-
-# Loop through data
-process_array() {
-    for row in "${data[@]}"; do
-        printf "%-12s %-10s %-10s\n" $row
-    done
-}
-
+# Print the table to verify (for debugging)
+#echo "$html_table"
+email_body=$(cat <<EOF
+<html>
+<body>
+<p>Dear user,</p>
+<p>The file count for Team298 workspaces:</p>
+$html_table
+</body>
+</html>
 EOF
+)
+recipients=("louiseegrimble@gmail.com" "nlg143@newcastle.ac.uk")
+#recipients=("nlg143@newcastle.ac.uk" "daniela.basurto-lozada@newcastle.ac.uk" "Dave.Horsfall@newcastle.ac.uk" "vm>
+subject="filecount"
+# Message for the email
+{
+    echo "Subject: $subject"
+    echo "To: ${recipients[*]}"
+    echo "Content-Type: text/html"
+    echo
+    echo "$email_body"
+} | sendmail "${recipients[@]}"
+# Message to confirm email has been sent to email
+echo "Script completed. Email sent to ${recipients[@]}"
 
 
 #echo "Submitted LSF job for filecount command."
