38a6421835549c7db9f9a6600bdb2e20d487b3eb
Louise Grimble <louiseegrimble@gmail.com>
2024-12-11 16:48:55 +0000

    adding logic to submit.sh #38

--- bin/irods/iget-cellranger/submit.sh
+++ bin/irods/iget-cellranger/submit.sh
@@ -49,13 +49,13 @@ IFS=',' read -r -a SAMPLES <<< "$SAMPLE_IDS"
 NUM_SAMPLES=${#SAMPLES[@]}
 
 # Check if irods_path.csv exists
-if [ -f irods_path.csv ]; then
+if [ -f "\$OUTPUT_DIR/irods_path.csv" ]; then
     # Extract base names from irods_path.csv
-    OUTS=$(sed 's|.*/||' irods_path.csv)
+    OUTS=\$(sed 's|.*/||' irods_path.csv)
 
     # Check if cellranger outputs for $SAMPLE already exist
-    if [ -d "$OUTPUT_DIR/$OUTS" ]; then
-        echo "Output directory '$OUTPUT_DIR/$OUTS' already contains cellranger outputs for sample $SAMPLE. Skipping job submission."
+    if [ -d "\$OUTPUT_DIR/\$OUTS" ]; then
+        echo "Output directory '\$OUTPUT_DIR/\$OUTS' already contains cellranger outputs for sample $SAMPLE. Skipping job submission."
         exit 0
     fi
 fi
@@ -114,11 +114,11 @@ echo "Saved \$num_paths matching path(s) to irods_path.csv."
 # check if cellranger outputs for $SAMPLE have already been pulled, exit
 #OUTS=\$(sed 's|.*/||' irods_path.csv)
 
-#[ -d "$OUTPUT_DIR/$OUTS" ]
-#if [ -d "\$OUTPUT_DIR/\$OUTS" ]; then
-#    echo "Output directory '\$OUTPUT_DIR' already contains cellranger outputs from iRODS. Exiting"
-#    exit 0
-#fi
+[ -d "$OUTPUT_DIR/$OUTS" ]
+if [ -d "\$OUTPUT_DIR/\$OUTS" ]; then
+    echo "Output directory '\$OUTPUT_DIR' already contains cellranger outputs from iRODS. Exiting"
+    exit 0
+fi
 
 # Read each line from irods_path.csv and use iget to pull files to the output dir
 while IFS= read -r irods_path; do
