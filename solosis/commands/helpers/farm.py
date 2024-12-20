fb4e39177e402e6203f537988752b242738aa9de
Dave Horsfall <dave@horsfall.dev>
2024-12-20 10:06:06 +0000

    formatting

--- solosis/commands/helpers/farm.py
+++ solosis/commands/helpers/farm.py
@@ -4,8 +4,14 @@ import click
 
 
 def farm(function):
-    function = click.option("--mem", default=50000, type=int, help="mem in MB")(function)
+    function = click.option("--mem", default=50000, type=int, help="mem in MB")(
+        function
+    )
     function = click.option("--cores", default=4, type=int, help="# of cores")(function)
-    function = click.option("--time", default="12:00", type = str, help="time for running")(function)
-    function = click.option("--queue", default="normal", type = str, help="queue name")(function)
+    function = click.option(
+        "--time", default="12:00", type=str, help="time for running"
+    )(function)
+    function = click.option("--queue", default="normal", type=str, help="queue name")(
+        function
+    )
     return function
