7ed0bb5150db6dc6199f2bb5a4e6f6975b774f5a
Louise Grimble <louiseegrimble@gmail.com>
2025-01-30 11:19:21 +0000

    remove emails while testing #90

--- bin/filesystem/disk-usage/submit.sh
+++ bin/filesystem/disk-usage/submit.sh
@@ -80,7 +80,7 @@ Please review contents of Lustre directory (/lustre/scratch126/cellgen/team298),
 #  this will change to if $used_value is more than 47.5T
 if [ "$nfs_used_int" -gt "$warn_int" ]; then
     # Submit the email
-    echo -e "$message_lustre" | mail -s "Lustre Quota Alert" nlg143@newcastle.ac.uk daniela.basurto-lozada@newcastle.ac.uk Dave.Horsfall@newcastle.ac.uk vm11@sanger.ac.uk
+    echo -e "$message_lustre" | mail -s "Lustre Quota Alert" nlg143@newcastle.ac.uk #daniela.basurto-lozada@newcastle.ac.uk Dave.Horsfall@newcastle.ac.uk vm11@sanger.ac.uk
 fi
 
 ###### nfs quota script ######
