6eff77eee250f2d43e2d898dd409937de963882d
Vijaya Baskar <vjbaskar@gmail.com>
2025-02-03 15:32:03 +0000

    Clean up

--- solosis/commands/farm/single_job.py
+++ solosis/commands/farm/single_job.py
@@ -6,7 +6,7 @@ from solosis.utils import echo_lsf_submission_message, echo_message, log_command
 
 # Script directory in the solosis package
 script_dir = os.path.dirname(os.path.abspath(__file__))  
-
+codebase = os.path.join(script_dir, "..", "..", "..", "bin")
 
 ## Functions supporting farm submission
 def bash_submit(job_runner: str, **kwargs) -> None:
@@ -33,7 +33,7 @@ def _single_command_bsub(command_to_exec, job_name, queue, time, cores, mem, **k
     Run a single command on the farm.
     """
     job_runner = os.path.abspath(
-        os.path.join(script_dir, "../../../bin/farm/single_job.sh")
+        os.path.join(codebase, "/farm/single_job.sh")
         )
     if len(command_to_exec) == 0:
         echo_message("No command to execute", type="error")
@@ -68,37 +68,12 @@ def single_cmd(ctx, command_to_exec, job_name, **kwargs):
     mem = kwargs.get("mem")
     log_command(ctx)
     if job_name == "default":
-        job_name = f"{ctx.command_path}_{ctx.obj['execution_id']}"
+        job_name = f"{ctx.obj['execution_id']}"
     else:
         job_name = f"{job_name}_{ctx.obj['execution_id']}"
-    echo_lsf_submission_message(f"Job name :{job_name} submitted to queue: {queue}")
+    echo_message(f"Job name :{job_name} submitted to queue: {queue}")
     _single_command_bsub(command_to_exec, job_name=job_name, **kwargs)
 
-
-@click.command("run_ipynb")
-@helpers.farm
-@click.option(
-    "-n", "--notebook", required=True, type=str, help="Path to the notebook to run"
-)
-@click.option(
-    "-j",
-    "--job_name",
-    required=False,
-    type=str,
-    help="Name of the job",
-)  # random val to be implemented (from import random)
-
-
-def run_ipynb(notebook, job_name, **kwargs):
-    """
-    Run a jupyter notebook on the farm.
-    """
-    job_runner = os.path.join(codebase, "single_job.sh")
-    # To do: Install jupyter in base solosis conda env
-    command_to_exec = f"source ~/.bashrc && conda activate /software/cellgen/team298/shared/envs/hl-conda/hl_minimal_v1.0.0 && jupyter nbconvert --to notebook --execute {notebook}"
-    bash_submit(
-        job_runner, command_to_exec=command_to_exec, job_name="notebook", **kwargs
-    )
 
 
 if __name__ == "__main__":
