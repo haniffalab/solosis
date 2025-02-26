f269b5d54c732ea455f26ab4072e359c86c786ee
Vijaya Baskar <vjbaskar@gmail.com>
2025-02-26 06:40:15 +0000

    Aligned sc-rna scanpy to the new refactored code base

--- solosis/commands/sc_rna/scanpy.py
+++ solosis/commands/sc_rna/scanpy.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os
 import subprocess
-
 import click
+from solosis.utils import irods_validation, log_command, echo_message
 
 
 @click.command("scanpy")
@@ -13,7 +13,8 @@ import click
     default="/lustre/scratch126/cellgen/team298/sample_data/",
     help="sample database folder",
 )
-def cmd(samplefile, sample_basedir):
+@click.pass_context
+def cmd(ctx, samplefile, sample_basedir):
     """
     Basic Scanpy workflow for scRNA-seq data, generates Jupyter Notebook ... \n
     ----------------------
@@ -21,14 +22,22 @@ def cmd(samplefile, sample_basedir):
     Example: /lustre/scratch126/cellgen/team298/soft/bin/examples/irods_download.txt
     Input file should have 3 mandatory columns:
     1st column: sanger_id, 2nd column: sample_name, LAST column: irods path
-
     """
-    shell_script = os.path.join(
-        os.getcwd(),
-        "bin/scrna/scanpy/submit.sh",
+    log_command(ctx)
+    echo_message(
+        f"Starting Process: {click.style(ctx.command.name, bold=True, underline=True)}",
+        "info",
     )
+
+     # Path to the script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    shell_script = os.path.abspath(
+        os.path.join(script_dir, "../../../bin/scrna/scanpy/submit.sh")
+    )
+    env = os.environ.copy()
+    env["solosis_dir"] = script_dir
     result = subprocess.run(
-        [shell_script, sample_basedir, samplefile], capture_output=True, text=True
+        [shell_script, sample_basedir, samplefile], env = env, capture_output=True, text=True
     )
-    click.echo(result.stdout)
-    click.echo(result.stderr)
+    echo_message(result.stdout)
+    echo_message(result.stderr)
