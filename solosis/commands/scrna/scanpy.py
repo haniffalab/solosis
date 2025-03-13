a7948c207080c148551725d9faae8492584ef3c3
Vijaya Baskar <vjbaskar@gmail.com>
2025-03-13 16:05:03 +0000

    Cleaned up function

--- solosis/commands/scrna/scanpy.py
+++ solosis/commands/scrna/scanpy.py
@@ -5,27 +5,35 @@ import subprocess
 import click
 
 from solosis.utils.farm import echo_message, irods_validation, log_command
-
+from solosis.utils.logging_utils import debug
+from solosis.utils.lsf_utils import lsf_job, lsf_options_sm, _assign_job_name
+from solosis.utils.env_utils import create_solosis_dirs
 
 @click.command("scanpy")
-@click.option("--samplefile", required=True, help="Sample file text file")
+
+@click.option("--samplefile", 
+              required=True, 
+              type=click.Path(exists=True),
+              help="Sample file text file")
 @click.option(
     "--sample_basedir",
     required=False,
-    default="/lustre/scratch126/cellgen/team298/sample_data/",
+    default="/lustre/scratch126/cellgen/team298/data/samples/",
     help="sample database folder",
 )
+@lsf_job(mem=64000)
 @click.pass_context
-def cmd(ctx, samplefile, sample_basedir):
+def cmd(ctx, samplefile, sample_basedir, **kwargs):
     """
     Basic Scanpy workflow for scRNA-seq data, generates Jupyter Notebook ... \n
     ----------------------
 
-    Example: /lustre/scratch126/cellgen/team298/soft/bin/examples/irods_download.txt
-    Input file should have 3 mandatory columns:
-    1st column: sanger_id, 2nd column: sample_name, LAST column: irods path
+    Example: (defunct) /lustre/scratch126/cellgen/team298/soft/bin/examples/irods_download.txt
+    Input file should have 1 mandatory columns:
+    1st column: sample_id
+    Any other column would be appended to your adata.obs dataframe
     """
-    log_command(ctx)
+    #log_command(ctx)
     echo_message(
         f"Starting Process: {click.style(ctx.command.name, bold=True, underline=True)}",
         "info",
@@ -36,11 +44,15 @@ def cmd(ctx, samplefile, sample_basedir):
     shell_script = os.path.abspath(
         os.path.join(script_dir, "../../../bin/scrna/scanpy/submit.sh")
     )
+    create_solosis_dirs()
+    #_assign_job_name("scanpy", ctx)
+
     env = os.environ.copy()
     env["solosis_dir"] = script_dir
+
     result = subprocess.run(
         [shell_script, sample_basedir, samplefile],
-        env=env,
+        env={**env, **kwargs},
         capture_output=True,
         text=True,
     )
