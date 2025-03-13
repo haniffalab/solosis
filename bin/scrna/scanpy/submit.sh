97b066104e1de41e846f81ce64209c255484b391
Vijaya Baskar <vjbaskar@gmail.com>
2025-03-13 16:06:06 +0000

    Edited the submission script to work with @lsf_job decorator

--- bin/scrna/scanpy/submit.sh
+++ bin/scrna/scanpy/submit.sh
@@ -15,7 +15,7 @@ sample_tsv=$1; shift
 
 #mkdir -p pap
 
-mem=50000
+#mem=50000
 target_dir=`pwd` # This is obtained by module load hl
 cwd=`pwd`
 run_token=$RUN_TOKEN
@@ -39,9 +39,14 @@ do
     outpt_folder="$cwd/rna_scanpy/"
 	mkdir -p $outpt_folder
 	echo "($i) [Info] $sample_name will be processed in $outpt_folder"
-    echo "$solosis_dir"
 	#="papermill $solosis_dir/../../../notebooks/sc_base1.ipynb $outpt_folder/$sample_name.ipynb  -p samples_database '${samples_database}' -p sample_name $sample_name -k python3  --log-output"
-    cmd="conda activate $conda_env &&  papermill $solosis_dir/../../../notebooks/sc_base1.ipynb $outpt_folder/${sample_name}_${sample_id}.ipynb  -p samples_database '${samples_database}' -p sample_name $sample_name -p sample_id $sample_id -p cellranger_folder cellranger/$cellranger_folder --log-output"
+    cmd="conda activate $conda_env && \
+    papermill $solosis_dir/../../../notebooks/sc_base1.ipynb $outpt_folder/${sample_name}_${sample_id}.ipynb \
+     -p samples_database '${samples_database}' \
+     -p sample_name $sample_name \
+     -p sample_id $sample_id \
+     -p cellranger_folder cellranger/$cellranger_folder \
+     -p sample_file $sample_tsv --log-output"
 	echo $cmd >> $ofile
     fi
 done < $sample_tsv
@@ -61,8 +66,14 @@ cat > $bsub_id.bsub <<EOF
 #BSUB -J ${bsub_id}_[1-$total_jobs]%20
 #BSUB -o $lsf_folder/${bsub_id}_%I.out
 #BSUB -e $lsf_folder/${bsub_id}_%I.err
+
+
+#BSUB -q $queue
+#BSUB -n $cpu  
 #BSUB -M $mem
 #BSUB -R "select[mem>$mem] rusage[mem=$mem]"
+#BSUB -W $time
+#BSUB -Ep /software/cellgen/cellgeni/etc/notify-slack.sh # CellGen Slack notification support
 conda activate $conda_env
 COMMAND=\$(sed -n "\${LSB_JOBINDEX}p" $ofile) 
 eval \$COMMAND
