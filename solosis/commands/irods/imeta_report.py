5630c4f7492b9b8715ff02b1e2e85ac5b3c1e17f
Louise Grimble <louiseegrimble@gmail.com>
2025-03-18 12:37:37 +0000

    combining imeta reports to create overall report #119

--- solosis/commands/irods/imeta_report.py
+++ solosis/commands/irods/imeta_report.py
@@ -59,6 +59,7 @@ def cmd(sample, samplefile, debug):
             cellranger = len(df[df["collection_type"] == "CellRanger"])
             data.append([sample, crams, cellranger])
 
+        # Display summary table for individual samples
         headers = [
             "Sample",
             "CRAM",
@@ -68,6 +69,27 @@ def cmd(sample, samplefile, debug):
             data, headers, tablefmt="pretty", numalign="left", stralign="left"
         )
         logger.info(f"Summary table... \n{table}")
+
+    execution_dir = os.getcwd()
+    all_report_path = os.path.join(execution_dir, "all-imeta-report.csv")
+    all_data = []
+
+    for sample in samples:
+        report_path = os.path.join(
+            os.getenv("TEAM_SAMPLES_DIR"), sample, "imeta_report.csv"
+        )
+        if os.path.exists(report_path):
+            df = pd.read_csv(report_path, header=None, names=["file_type", "path"])
+            df.insert(0, "sample", sample)  # Add the sample name as the first column
+            all_data.append(df)
+
+    # Combine all sample data into a single CSV report
+    if all_data:
+        combined_df = pd.concat(all_data, ignore_index=True)
+        combined_df.to_csv(all_report_path, index=False)
+        logger.info(f"✅ Overall report generated: {all_report_path}")
+    else:
+        logger.warning("⚠ No reports found. 'all-imeta-report.csv' was not created.")
 
 
 if __name__ == "__main__":
