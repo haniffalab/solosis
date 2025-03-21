b78e952128318587a5002b2268db216622cb0cee
Dave Horsfall <dave@horsfall.dev>
2025-03-21 13:42:03 +0000

    bugfixes and simplify

--- solosis/commands/irods/imeta_report.py
+++ solosis/commands/irods/imeta_report.py
@@ -36,6 +36,7 @@ def cmd(sample, samplefile, debug):
         raise click.Abort()
 
     samples = collect_samples(sample, samplefile)
+
     data = []
     for sample in samples:
         logger.info(f"Processing sample: {sample}")
@@ -59,34 +60,20 @@ def cmd(sample, samplefile, debug):
             cellranger = len(df[df["collection_type"] == "CellRanger"])
             data.append([sample, crams, cellranger])
 
-        # Display summary table for individual samples
-        headers = [
-            "Sample",
-            "CRAM",
-            "CellRanger",
-        ]
-        table = tabulate(
-            data, headers, tablefmt="pretty", numalign="left", stralign="left"
-        )
-        logger.info(f"Summary table... \n{table}")
+    # Display summary table for samples
+    headers = [
+        "Sample",
+        "CRAM",
+        "CellRanger",
+    ]
+    table = tabulate(data, headers, tablefmt="pretty", numalign="left", stralign="left")
+    logger.info(f"Summary table... \n{table}")
 
+    # Output combined report for samples
     execution_dir = os.getcwd()
     all_report_path = os.path.join(execution_dir, "all-imeta-report.csv")
-    all_data = []
-
-    for sample in samples:
-        report_path = os.path.join(
-            os.getenv("TEAM_SAMPLES_DIR"), sample, "imeta_report.csv"
-        )
-        if os.path.exists(report_path):
-            df = pd.read_csv(report_path, header=None, names=["file_type", "path"])
-            df.insert(0, "sample", sample)  # Add the sample name as the first column
-            all_data.append(df)
-
-    # Combine all sample data into a single CSV report
-    if all_data:
-        combined_df = pd.concat(all_data, ignore_index=True)
-        combined_df.to_csv(all_report_path, index=False)
+    if data:
+        data.to_csv(all_report_path, index=False)
         logger.info(f"Overall report generated: {all_report_path}")
     else:
         logger.warning("No reports found. 'all-imeta-report.csv' was not created.")
