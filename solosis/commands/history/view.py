bae6d76d75c99b854a8e9938fe7736543f01a0cf
Louise Grimble <louiseegrimble@gmail.com>
2025-03-26 15:55:23 +0000

    exclude history command with additional flags too  #116

--- solosis/commands/history/view.py
+++ solosis/commands/history/view.py
@@ -43,7 +43,7 @@ def cmd(lines, debug):
         commands = command.split(" ", 1)[-1]
 
         # Exclude `./solosis-cli history view` commands
-        if command.strip() == "./solosis-cli history view":
+        if command.startswith() == "./solosis-cli history view":
             continue
 
         table_data.append(
