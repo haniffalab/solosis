517485b5659030b754f259d687f1971434b9433f
Louise Grimble <louiseegrimble@gmail.com>
2025-04-25 11:14:07 +0100

    init chemistry flag #163

--- solosis/commands/alignment/cellranger_count.py
+++ solosis/commands/alignment/cellranger_count.py
@@ -146,7 +146,7 @@ def cmd(
         logger.debug(f"Temporary command file created: {tmpfile.name}")
         os.chmod(tmpfile.name, 0o660)
         for sample in valid_samples:
-            command = f"{cellranger_count_path} {sample['sample_id']} {sample['output_dir']} {sample['fastq_dir']} {chemistry} {version} {cpu} {mem}"
+            command = f"{cellranger_count_path} {sample['sample_id']} {sample['output_dir']} {sample['fastq_dir']} {version} {cpu} {mem}"
             if not create_bam:
                 command += " --no-bam"
             tmpfile.write(command + "\n")
